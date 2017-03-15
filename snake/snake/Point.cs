e00f95a4746c431ff1bd34400dc16bf4c85a720e
yaneproger@gmail.com <yaneproger@gmail.com>
2017-03-15 12:25:23 +0400

    4.Инкапсуляция

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -8,11 +8,24 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
+
     class Point
     {
         public int x;
         public int y;
         public char sym;
+
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)
+
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
 
         public void Draw()
         {
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -12,16 +12,16 @@ namespace Snake
         static void Main( string[] args)
         {
             
-            Point p1 = new Point();
-            p1.x = 1;
+            Point p1 = new Point(1, 3, '*');
+            /*p1.x = 1;
             p1.y = 3;
-            p1.sym = '*';
+            p1.sym = '*'; */
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
+            Point p2 = new Point(4, 5, '#');
+            /*p2.x = 4;
             p2.y = 5;
-            p2.sym = '#';
+            p2.sym = '#';*/
             p2.Draw();
 
 
